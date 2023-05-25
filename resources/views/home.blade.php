9330308d08f8dd6fe4e10173f12447f21ff7b1d7
John Doe <yamatogija@gmail.com>
2023-05-25 14:28:46 +0700

    Add Event at Homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,8 @@
                 <div class="w-full h-full mx-auto overflow-x-hidden overflow-y-hidden">
                     <div id="slider"
                         class="flex items-center justify-start h-full transition duration-700 ease-out lg:gap-8 md:gap-6 gap-14">
-                        <div aria-label="cards" class="w-full overflow-hidden bg-white rounded shadow sm:w-2/3 lg:w-1/2">
+                        <div aria-label="cards"
+                            class="flex-shrink-0 w-full overflow-hidden bg-white rounded shadow sm:w-2/3 lg:w-1/2">
                             <div class="relative w-full">
                                 <img tabindex="0"
                                     class="object-cover object-center w-full h-48 rounded-t shadow focus:outline-none"
@@ -36,11 +37,52 @@
                                 <div class="w-full ">
                                     <h2 class="mb-3 text-2xl font-medium tracking-normal xl:mb-0 xl:mr-4">
                                         Marshall Mathers</h2>
-                                    {{-- <div
-                                        class="flex flex-col items-center justify-between mb-3 text-center xl:text-left xl:mb-0 xl:flex-row xl:justify-start">
-                                        <a tabindex="0" class="text-gray-800 focus:outline-none ">
-                                        </a>
-                                    </div> --}}
+                                    <p tabindex="0"
+                                        class="mt-2 mb-4 text-sm leading-5 tracking-normal text-gray-600 focus:outline-none xl:text-left">
+                                        HI, I am a direct response copywriter from the US. When you work with me, we
+                                        have the same goal. Maximizing your ROI
+                                    </p>
+                                    <a href=""
+                                        class="inline-block mt-auto mb-2 font-semibold underline cursor-pointer text-slate-500">Baca
+                                        Selengkapnya <i class="ml-2 fa-solid fa-up-right-from-square"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div aria-label="cards"
+                            class="flex-shrink-0 w-full overflow-hidden bg-white rounded shadow sm:w-2/3 lg:w-1/2">
+                            <div class="relative w-full">
+                                <img tabindex="0"
+                                    class="object-cover object-center w-full h-48 rounded-t shadow focus:outline-none"
+                                    src="https://tuk-cdn.s3.amazonaws.com/assets/components/grid_cards/gc_29.png"
+                                    alt="mountains cover" />
+                            </div>
+                            <div class="w-full px-5 pt-2 pb-6 xl:px-7">
+                                <div class="w-full ">
+                                    <h2 class="mb-3 text-2xl font-medium tracking-normal xl:mb-0 xl:mr-4">
+                                        Marshall Mathers</h2>
+                                    <p tabindex="0"
+                                        class="mt-2 mb-4 text-sm leading-5 tracking-normal text-gray-600 focus:outline-none xl:text-left">
+                                        HI, I am a direct response copywriter from the US. When you work with me, we
+                                        have the same goal. Maximizing your ROI
+                                    </p>
+                                    <a href=""
+                                        class="inline-block mt-auto mb-2 font-semibold underline cursor-pointer text-slate-500">Baca
+                                        Selengkapnya <i class="ml-2 fa-solid fa-up-right-from-square"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div aria-label="cards"
+                            class="flex-shrink-0 w-full overflow-hidden bg-white rounded shadow sm:w-2/3 lg:w-1/2">
+                            <div class="relative w-full">
+                                <img tabindex="0"
+                                    class="object-cover object-center w-full h-48 rounded-t shadow focus:outline-none"
+                                    src="https://tuk-cdn.s3.amazonaws.com/assets/components/grid_cards/gc_29.png"
+                                    alt="mountains cover" />
+                            </div>
+                            <div class="w-full px-5 pt-2 pb-6 xl:px-7">
+                                <div class="w-full ">
+                                    <h2 class="mb-3 text-2xl font-medium tracking-normal xl:mb-0 xl:mr-4">
+                                        Marshall Mathers</h2>
                                     <p tabindex="0"
                                         class="mt-2 mb-4 text-sm leading-5 tracking-normal text-gray-600 focus:outline-none xl:text-left">
                                         HI, I am a direct response copywriter from the US. When you work with me, we
@@ -54,32 +96,47 @@
                         </div>
                     </div>
                 </div>
-                {{-- <button aria-label="slide backward"
-                    class="absolute left-0 z-30 ml-10 cursor-pointer focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
+            </div>
+            <div class="flex justify-center w-full gap-x-4">
+                <button aria-label="slide backward"
+                    class="cursor-pointer focus:outline-none bg-gradient-to-b from-premier from-20% shadow-md hover:shadow-xl transition-all px-3 py-2 rounded-md to-orange-600"
                     id="prev">
-                    <svg class="dark:text-gray-900" width="8" height="14" viewBox="0 0 8 14" fill="none"
+                    <svg class="font-bold text-white" width="8" height="14" viewBox="0 0 8 14" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <path d="M7 1L1 7L7 13" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                             stroke-linejoin="round" />
                     </svg>
                 </button>
                 <button aria-label="slide forward"
-                    class="absolute right-0 z-30 mr-10 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
+                    class="cursor-pointer focus:outline-none bg-gradient-to-b from-premier from-20% shadow-md hover:shadow-xl transition-all px-3 py-2 rounded-md to-orange-600"
                     id="next">
-                    <svg class="dark:text-gray-900" width="8" height="14" viewBox="0 0 8 14" fill="none"
+                    <svg class="font-bold text-white" width="8" height="14" viewBox="0 0 8 14" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <path d="M1 1L7 7L1 13" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                             stroke-linejoin="round" />
                     </svg>
-                </button> --}}
-                {{-- <div class="relative flex items-center justify-center w-full">
-                </div> --}}
-                {{-- <div class="flex items-center justify-center w-full h-full px-4 py-24 sm:py-8">
-                </div> --}}
-
-
-
-
+                </button>
+            </div>
+        </section>
+        <section id="event" class="w-full px-3 mt-12">
+            <span class="block text-xl text-white underline sm:text-2xl md:text-4xl font-batik">Event Terdekat</span>
+            <div class="flex flex-col gap-4 mt-3">
+                <div
+                    class="flex justify-between  p-3 font-semibold text-white transition-all rounded-md hover:shadow-lg hover:to-orange-600 duration-1000 ease-in-out hover:from-40% bg-gradient-to-r from-premier to-premier/70">
+                    <div class="grid grid-cols-2">
+                        <span class="">23 Maret 2023 - 24 April 2023</span>
+                        <span class="text-left truncate">Kunjungan Ke DPW Sepakat DKI Jakarta</span>
+                    </div>
+                    <i class="my-auto text-right align-middle fa-solid fa-angle-right"></i>
+                </div>
+                <div
+                    class="flex justify-between  p-3 font-semibold text-white transition-all rounded-md hover:shadow-lg hover:to-orange-600 duration-1000 ease-in-out hover:from-40% bg-gradient-to-r from-premier to-premier/70">
+                    <div class="grid grid-cols-2">
+                        <span class="">23 Maret 2023 - 24 April 2023</span>
+                        <span class="text-left truncate">Kunjungan Ke DPW Sepakat DKI Jakarta</span>
+                    </div>
+                    <i class="my-auto text-right align-middle fa-solid fa-angle-right"></i>
+                </div>
             </div>
         </section>
     </main>
